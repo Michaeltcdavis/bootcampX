100107479b5773ab37cbc5510de3a1649021d234
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-10-17 23:20:07 +0000

    added some queries using GROUP BY and HAVING

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS cohorts_name, COUNT(students.*) AS student_count
+FROM students
+JOIN cohorts ON students.cohort_id=cohorts.id
+GROUP BY cohorts.name
+HAVING COUNT(students.*) >= 18
+ORDER BY COUNT(students.*);
